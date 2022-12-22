3b1053c1827de0410d27ec5e833328bad1b39edf
Official Husko <official.husko@gmail.com>
2022-12-22 22:26:39 +0100

    some backend work and file restructering

--- website/views.py
+++ website/views.py
@@ -12,8 +12,15 @@ background = random.choice(background_list)
 def blank():
     session["background"] = background
     if os.path.exists("settings.json"):
-        return redirect(url_for("views.setup"))
-        #return redirect(url_for("views.welcome"))
+        with open("settings.json", "r") as settings:
+            user_settings = json.load(settings)
+            session["default_author"] = user_settings["default_author"]
+            session["default_author_short"] = user_settings["default_author_short"]
+            session["language"] = user_settings["language"]
+            session["default_github"] = user_settings["default_github"]
+            session["versions"] = user_settings["versions"]
+            print(session["versions"])
+        return redirect(url_for("views.welcome"))
     else:
         return redirect(url_for("views.setup"))
 
@@ -22,27 +29,28 @@ def setup():
     session["background"] = background
     if request.method == "POST":
         data = json.loads(str(request.form.to_dict(flat=True)).replace("'", '"'))
-        
         data_example = {
-            "versions": data["target-versions"].replace(" ", "").split(","),
+            "versions": data["target-versions"].translate(str.maketrans({"'": "", " ": ""})).split(","),
             "default_author": data["default-author"],
             "default_author_short": data["default-author-short-form"],
             "language": data["language"],
             "default_github": data["default-github-username"]
         }
-        
         settings = open("settings.json", "w")
-        json.dump(data_example, settings, indent=2)
-        # TODO: write setup settings to settings.json
+        json.dump(data_example, settings, indent=6)
         flash("Setup Completed!", category="success")
         sleep(3)
-        #return redirect(url_for("views.welcome"))
+        return redirect(url_for("views.welcome"))
     
     return render_template("setup.html")
 
 @views.route("/welcome")
 def welcome():
     session["background"] = background
+    try:
+        session["default_author"]
+    except:
+        return redirect(url_for("views.blank"))
     # TODO: Show all found projects or just a simple welcome
     return render_template("welcome.html")
 
